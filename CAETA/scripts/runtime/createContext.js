7adac5728668928523850adc40090108e38897fb
etudism <etudism@gmail.com>
2026-04-13 23:30:14 +0900

    Delete CAETA/scripts directory

--- CAETA/scripts/runtime/createContext.js
+++
@@ -1,7 +0,0 @@
-import * as THREE from 'https://esm.sh/three@0.161.0';
-import { OrbitControls } from 'https://esm.sh/three@0.161.0/examples/jsm/controls/OrbitControls.js';
-import { DATA } from '../data.js';
-
-export function createRuntimeContext() {
-  return { THREE, OrbitControls, DATA, window, document, console, history, localStorage, URL, Math, Path2D, requestAnimationFrame, setTimeout, clearTimeout, performance };
-}
